1617916f05ee005a0f7a85f3d9d035e54629f796
swills1 <steven@wills.me>
2022-10-27 23:34:16 -0500

    Delete temp directory

--- temp/block.py
+++
@@ -1,4 +0,0 @@
-from dnsblock import ingest
-
-ingest.build_conf_file('/home/steven/Projects/packages/dnsblock/temp/cblocklist.txt', 'local-zone: "', '" always_refuse')
-#local-zone: "m2.ai" always_refuse
